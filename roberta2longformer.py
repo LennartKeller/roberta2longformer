bef25291cf0a8f3843e9b52e601b4685e626a951
Lennart Keller <lennart.keller@googlemail.com>
2021-12-15 14:41:00 +0100

    copy positional embeddings too

--- roberta2longformer.py
+++ roberta2longformer.py
@@ -1,20 +1,24 @@
+import torch
+from torch import nn
+
 from collections import OrderedDict
 from tempfile import TemporaryDirectory
 from transformers import LongformerModel, LongformerConfig, LongformerTokenizerFast
-from torch import nn
 
 
 def convert_roberta_to_longformer(
     roberta_model,
     roberta_tokenizer,
     longformer_max_length: int = 4096,
+    attention_window: int = 512,
 ):
 
     ##################################
     # Create new longformer instance #
     ##################################
     longformer_config = LongformerConfig(
-        max_position_embeddings=longformer_max_length + 2, token_type_ids=1
+        max_position_embeddings=longformer_max_length + 2,
+        attention_window=attention_window,
     )
     longformer_model = LongformerModel(longformer_config)
 
@@ -41,7 +45,7 @@ def convert_roberta_to_longformer(
     # Pooler weights are kept untouched.
 
     # ---------#
-    # Encoder #
+    # Encoder  #
     # ---------#
     roberta_parameters = roberta_model.encoder.state_dict()
     longformer_parameters = longformer_model.encoder.state_dict()
@@ -84,12 +88,12 @@ def convert_roberta_to_longformer(
     longformer_model.encoder.load_state_dict(longformer_parameters, strict=True)
 
     # ------------#
-    # Embeddings #
+    # Embeddings  #
     # ------------#
     # There are two types of embeddings:
+
     # 1. Token embeddings
-    # 2. Positional embeddings
-    # We only need to copy the token embeddings.
+    # We can simply copy the token embeddings.
 
     # We have to resize the token embeddings upfront, to make load_state_dict work.
     longformer_model.resize_token_embeddings(len(roberta_tokenizer))
@@ -100,8 +104,43 @@ def convert_roberta_to_longformer(
     for key, item in roberta_embeddings_parameters.items():
         if not "position" in key and not "token_type_embeddings" in key:
             embedding_parameters2copy.append((key, item))
-    embedding_parameters2copy = OrderedDict(embedding_parameters2copy)
 
+    # 2. Positional embeddings
+    # The positional embeddings are repeatedly copied over
+    # to longformer to match the new max_seq_length
+
+    roberta_pos_embs = roberta_model.embeddings.state_dict()[
+        "position_embeddings.weight"
+    ][:-2]
+    roberta_pos_embs_extra = roberta_model.embeddings.state_dict()[
+        "position_embeddings.weight"
+    ][-2:]
+
+    assert (
+        roberta_pos_embs.size(0) < longformer_max_length
+    ), "Longformer sequence length has to be longer than roberta original sequence length"
+
+    # Figure out how many time we need to copy the original embeddings
+    n_copies = round(longformer_max_length / roberta_pos_embs.size(0))
+
+    # Copy the embeddings and handle the last missing ones.
+    longformer_pos_embs = pos_embs.repeat((n_copies, 1))
+    n_pos_embs_left = longformer_max_length - longformer_pos_embs.size(0)
+    longformer_pos_embs = torch.cat(
+        [longformer_pos_embs, pos_embs[:n_pos_embs_left]], dim=0
+    )
+
+    # Add the last extra embeddings.
+    longformer_pos_embs = torch.cat(
+        [longformer_pos_embs, roberta_pos_embs_extra], dim=0
+    )
+
+    embedding_parameters2copy.append(
+        ("position_embeddings.weight", longformer_pos_embs)
+    )
+
+    # Load the embedding weights into the longformer model
+    embedding_parameters2copy = OrderedDict(embedding_parameters2copy)
     longformer_model.embeddings.load_state_dict(embedding_parameters2copy, strict=False)
 
     return longformer_model, longformer_tokenizer
